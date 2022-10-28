56f760507b06fad0b147b02231f33d95412e06f8
miresque <mirkilinc@gmail.com>
2022-10-28 01:00:52 +0100

    feat: add live game-client api requests

---
+++ src/utils/apiRequests.jsx
@@ -0,0 +1,53 @@
+import axios from 'axios';
+// const apiKey = process.env.REACT_APP_API_KEY;
+
+export const getTooltipData = async () => {  
+  const currentPlayer = await getCurrentPlayerData();
+  const abilities = await getChampionAbilities(currentPlayer);
+  const summonerSpells = await getSummonerSpells(currentPlayer);
+
+  const response = {
+    championName: currentPlayer.championName,
+    abilities: abilities, // //get info using champion name
+    summonerSpells: summonerSpells, //get info using champion name
+    items: [],
+    runes: {}
+  }
+
+  return response;
+};
+
+const getActivePlayerName = async () => {
+  const link = '/liveclientdata/activeplayername';
+  return await axios.get(link)
+}
+
+const getCurrentPlayerData = async () => {
+  const gameDataLink = '/liveclientdata/allgamedata';
+  const gameDataResponse = await axios.get(gameDataLink);
+  const summonerName = await getActivePlayerName();
+  
+  return gameDataResponse.data.allPlayers.find(player => player.summonerName === summonerName.data)
+}
+
+const getChampionAbilities = async (currentPlayer) => {
+  const championName = currentPlayer.championName;
+  const champDataLink = `http://ddragon.leagueoflegends.com/cdn/12.20.1/data/en_US/champion/${championName}.json`;
+  const champDataLinkResponse = await axios.get(champDataLink);
+  return {
+    passive: champDataLinkResponse.data.data[championName].passive,
+    spells: champDataLinkResponse.data.data[championName].spells
+  };
+}
+
+const getSummonerSpells = async (currentPlayer) => {
+  const summonersLink = 'http://ddragon.leagueoflegends.com/cdn/12.20.1/data/en_US/summoner.json'
+  const summonersResponse = await axios.get(summonersLink)
+
+  const filteredSummonerSpellOne = Object.values(summonersResponse.data.data).find(summonerSpell => summonerSpell.name === currentPlayer.summonerSpells.summonerSpellOne.displayName)
+  const filteredSummonerSpellTwo = Object.values(summonersResponse.data.data).find(summonerSpell => summonerSpell.name === currentPlayer.summonerSpells.summonerSpellTwo.displayName)
+  return [
+    filteredSummonerSpellOne,
+    filteredSummonerSpellTwo,
+  ]
+}
